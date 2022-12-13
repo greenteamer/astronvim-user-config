9066b96530440938ca64d67abbf7e79a7a9e2e5d
aleks <teamer777@gmail.com>
2022-12-13 23:55:23 +0400

    config for additional libraries

--- init.lua
+++ init.lua
@@ -11,6 +11,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
         command = "set rnu nu",
 })
 
+vim.opt.rtp:append("/Users/aleks/.opam/default/share/ocp-indent/vim")
+
 
 local config = {
 
@@ -37,16 +39,22 @@ local config = {
         -- colorscheme = "default_theme",
         -- colorscheme = "ondedark",
         -- colorscheme = "gruvbox",
-        -- colorscheme = "dayfox",
+        -- colorscheme = "solarized",
+        -- colorscheme = "monokai_pro",
+        -- colorscheme = "OceanicNext",
         -- colorscheme = "catppuccin-latte",
-        colorscheme = "catppuccin-macchiato",
+        -- colorscheme = "catppuccin-macchiato",
         -- colorscheme = "catppuccin-mocha",
         -- colorscheme = "catppuccin-frappe",
         -- colorscheme = "onehalflight",
+        colorscheme = "xcodelight",
         -- colorscheme = "PaperColor",
         -- colorscheme = "space_vim_theme",
         -- colorscheme = "mac_classic",
+        -- colorscheme = "summerfruit256",
         -- colorscheme = "paper",
+        -- colorscheme = "dawnfox",
+        -- colorscheme = "oxocarbon",
 
         -- Add highlight groups in any theme
         highlights = {
@@ -59,39 +67,54 @@ local config = {
         },
 
         -- set vim options here (vim.<first_key>.<second_key> = value)
-        options = {
-                opt = {
-                        -- set to true or false etc.
-                        -- t_Co = 256,
-                        -- background = "light",
-                        relativenumber = true, -- sets vim.opt.relativenumber
-                        number = true, -- sets vim.opt.number
-                        spell = false, -- sets vim.opt.spell
-                        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-                        wrap = false, -- sets vim.opt.wrap
-                        list = true,
-                        listchars = { eol = '↲', tab = '▸ ', space = '·' },
-                },
-                g = {
-                        mapleader = ",", -- sets vim.g.mapleader
-                        autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-                        cmp_enabled = true, -- enable completion at start
-                        autopairs_enabled = true, -- enable autopairs at start
-                        diagnostics_enabled = true, -- enable diagnostics at start
-                        status_diagnostics_enabled = true, -- enable diagnostics in statusline
-                        icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-                        ui_notifications_enabled = false, -- disable notifications when toggling UI elements
-                },
-        },
+        -- options = {
+        --         opt = {
+        --                 -- set to true or false etc.
+        --                 -- syntax = "enable",
+        background = "light",
+        --                 -- background = "dark",
+        --                 -- termguicolors = true,
+        --                 relativenumber = true, -- sets vim.opt.relativenumber
+        --                 number = true, -- sets vim.opt.number
+        --                 spell = false, -- sets vim.opt.spell
+        --                 signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        --                 wrap = false, -- sets vim.opt.wrap
+        --                 -- list = true,
+        --                 list = false,
+        --                 listchars = { eol = '↲', tab = '▸ ', space = '·' },
+        --         },
+        --         g = {
+        --                 mapleader = ",", -- sets vim.g.mapleader
+        --                 autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+        --                 cmp_enabled = true, -- enable completion at start
+        --                 autopairs_enabled = true, -- enable autopairs at start
+        --                 diagnostics_enabled = true, -- enable diagnostics at start
+        --                 status_diagnostics_enabled = true, -- enable diagnostics in statusline
+        --                 icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+        --                 ui_notifications_enabled = false, -- disable notifications when toggling UI elements
+        --         },
+        -- },
         -- If you need more control, you can use the function()...end notation
-        -- options = function(local_vim)
-        --   local_vim.opt.relativenumber = true
-        --   local_vim.g.mapleader = " "
-        --   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
-        --   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
-        --
-        --   return local_vim
-        -- end,
+        options = function(local_vim)
+                local_vim.opt.relativenumber = true
+                local_vim.opt.number = true
+                local_vim.opt.spell = false
+                local_vim.opt.signcolumn = "auto"
+                local_vim.opt.list = false
+
+                local_vim.g.mapleader = ","
+                local_vim.g.autoformat_enabled = true
+                local_vim.g.cmp_enabled = true
+                local_vim.g.autopairs_enabled = true
+                local_vim.g.diagnostics_enabled = true
+                local_vim.g.status_diagnostics_enabled = true
+                local_vim.g.icons_enabled = true
+                local_vim.g.ui_notifications_enabled = false
+                -- local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+                -- local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+
+                return local_vim
+        end,
 
         -- Set dashboard header
         header = {
@@ -251,6 +274,16 @@ local config = {
                         --         end
                         --
                         -- },
+                        --
+
+                        -- ["jose-elias-alvarez/null-ls.nvim"] = {
+                        --         config = function()
+                        --                 require("null-ls").setup({
+                        --                         sources = { filetype = { "lua", "rust" } }
+                        --                 })
+                        --         end
+                        --
+                        -- },
 
                         -- You can also add new plugins here as well:
                         -- Add plugins, the packer syntax without the "use"
@@ -259,10 +292,17 @@ local config = {
                         { "liuchengxu/space-vim-theme" },
                         { "NLKNguyen/papercolor-theme" },
                         { "sonph/onehalf" },
+                        -- { "sonph/onehalf/tree/master/vim" },
                         { "ayu-theme/ayu-vim" },
                         { "joshdick/onedark.vim" },
                         { "morhetz/gruvbox" },
+                        { "phanviet/vim-monokai-pro" },
+                        { "mhartington/oceanic-next" },
                         { "catppuccin/nvim", as = "catppuccin" },
+                        { "vim-scripts/summerfruit256.vim" },
+                        { "shaunsingh/solarized.nvim" },
+                        { "arzg/vim-colors-xcode" },
+                        { 'nyoom-engineering/oxocarbon.nvim' },
                         { "justinmk/vim-sneak",
                                 -- config = function()
                                 --         require("vim-sneak").setup({
@@ -282,8 +322,6 @@ local config = {
                                         })
                                 end
                         },
-                        -- { "easymotion/vim-easymotion" },
-                        -- { "EdenEast/nightfox.nvim" },
                         {
                                 "EdenEast/nightfox.nvim",
                                 config = function()
